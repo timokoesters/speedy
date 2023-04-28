6d7d05f639613b5e01f116e388d551459343d34d
Timo Kösters <timo@koesters.xyz>
2023-04-28 15:08:29 +0200

    Restructure code, improve output format

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 use std::sync::{Arc, RwLock};
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 use console_engine::KeyCode;
 use signal_hook::{consts::SIGUSR1, iterator::Signals};
 
+struct Section {
+    name: String,
+    pb_total: Option<u32>,
+    current_total: Option<u32>,
+}
+
 struct App {
     // name, pb and current time in sections
-    sections: Vec<(String, Option<u32>, Option<u32>)>,
+    sections: Vec<Section>,
     current_section: usize,
     start_time: Instant,
+    running: bool,
 }
 
 impl App {
@@ -21,10 +28,134 @@ impl App {
         App {
             sections: sections
                 .iter()
-                .map(|&s| (s.to_owned(), None, None))
+                .map(|&s| Section {
+                    name: s.to_owned(),
+                    pb_total: None,
+                    current_total: None,
+                })
                 .collect(),
             current_section: 0,
             start_time: Instant::now(), // This is not the actual start, it will be reset later
+            running: false,
+        }
+    }
+
+    fn update_current_time(&mut self) {
+        if self.current_section >= self.sections.len() {
+            return;
+        }
+
+        self.sections[self.current_section].current_total =
+            Some(self.start_time.elapsed().as_millis() as u32);
+    }
+
+    fn current_total_time(&self, section: usize) -> String {
+        let s = &self.sections[section];
+        self.time_to_string(section, s.current_total)
+    }
+
+    fn pb_total_time(&self, section: usize) -> String {
+        let s = &self.sections[section];
+        self.fixed_time_to_string(section, s.pb_total)
+    }
+
+    fn current_section_time(&self, section: usize) -> String {
+        if section == 0 {
+            return self.current_total_time(section);
+        }
+
+        let s = &self.sections[section];
+        let last = &self.sections[section - 1];
+
+        let time = if let (Some(c), Some(l)) = (s.current_total, last.current_total) {
+            Some(c - l)
+        } else {
+            None
+        };
+
+        self.time_to_string(section, time)
+    }
+
+    fn pb_section_time(&self, section: usize) -> String {
+        if section == 0 {
+            return self.pb_total_time(section);
+        }
+
+        let s = &self.sections[section];
+        let last = &self.sections[section - 1];
+
+        let time = if let (Some(c), Some(l)) = (s.pb_total, last.pb_total) {
+            Some(c - l)
+        } else {
+            None
+        };
+
+        self.fixed_time_to_string(section, time)
+    }
+
+    fn delta_total_time(&self, section: usize) -> String {
+        let s = &self.sections[section];
+        let time = if let (Some(p), Some(c)) = (s.pb_total, s.current_total) {
+            Some(c as i32 - p as i32)
+        } else {
+            None
+        };
+
+        self.delta_time_to_string(section, time)
+    }
+
+    fn delta_section_time(&self, section: usize) -> String {
+        if section == 0 {
+            return self.delta_total_time(section);
+        }
+
+        let s = &self.sections[section];
+        let last = &self.sections[section - 1];
+
+        let time = if let (Some(pb_c), Some(pb_l), Some(c_c), Some(c_l)) = (
+            s.pb_total,
+            last.pb_total,
+            s.current_total,
+            last.current_total,
+        ) {
+            Some((pb_c - pb_l) as i32 - (c_c - c_l) as i32)
+        } else {
+            None
+        };
+
+        self.delta_time_to_string(section, time)
+    }
+
+    fn time_to_string(&self, section: usize, time: Option<u32>) -> String {
+        if let Some(t) = time {
+            format!("{:>2}:{:02}", t / 60000, (t / 1000) % 60)
+        } else {
+            if section < self.current_section {
+                "--:--".to_owned()
+            } else {
+                "     ".to_owned()
+            }
+        }
+    }
+
+    fn fixed_time_to_string(&self, section: usize, time: Option<u32>) -> String {
+        if let Some(t) = time {
+            format!("{:>2}:{:02}", t / 60000, (t / 1000) % 60)
+        } else {
+            "--:--".to_owned()
+        }
+    }
+
+    fn delta_time_to_string(&self, section: usize, time: Option<i32>) -> String {
+        if let Some(t) = time {
+            let sign = if t < 0 { '-' } else { '+' };
+            format!("({}{}:{:02})", sign, t / 60000, (t / 1000) % 60)
+        } else {
+            if section < self.current_section {
+                "(--:--)".to_owned()
+            } else {
+                "       ".to_owned()
+            }
         }
     }
 }
@@ -44,7 +175,8 @@ fn main() {
                 }
 
                 let current_section = app.current_section;
-                app.sections[current_section].2 = Some(app.start_time.elapsed().as_millis() as u32);
+                app.sections[current_section].current_total =
+                    Some(app.start_time.elapsed().as_millis() as u32);
                 app.current_section += 1;
             }
         }
@@ -55,63 +187,22 @@ fn main() {
         engine.wait_frame();
         engine.clear_screen();
 
-        engine.print(0, 0, "speedy: Portal1\nsection best  delta  current total");
-        let app = app.read().unwrap();
-        for (i, (name, pb, mut current_total)) in app.sections.iter().enumerate() {
-            let last_total = if i >= 1 {
-                app.sections[i - 1].2.unwrap_or(0)
-            } else {
-                0
-            };
-            let last_pb_total = if i > 1 {
-                app.sections[i - 1].1.unwrap_or(0)
-            } else {
-                0
-            };
+        engine.print(0, 0, "speedy: Portal1\nsection best  current      total");
+        let app = &mut app.write().unwrap();
+        app.update_current_time();
 
-            if i == app.current_section {
-                current_total = Some(app.start_time.elapsed().as_millis() as u32);
-            }
-            let delta_str = if let (Some(p), Some(c)) = (pb, current_total) {
-                let d = c as i32 - *p as i32;
-                let sign = if d < 0 { '-' } else { '+' };
-                format!("{}{}:{:02}", sign, d / 60000, (d / 1000) % 60)
-            } else {
-                if i < app.current_section {
-                    "-:--".to_owned()
-                } else {
-                    "".to_owned()
-                }
-            };
-
-            let pb_str = pb.map_or("-:--".to_owned(), |p| {
-                let p = p - last_pb_total;
-                format!("{}:{:2}", p / 60000, (p / 1000) % 60)
-            });
-
-            let current_str;
-            let current_total_str;
-
-            if let Some(c) = current_total {
-                let d = c - last_total;
-                current_str = format!("{}:{:02}", d / 60000, (d / 1000) % 60);
-                current_total_str = format!("{}:{:02}", c / 60000, (c / 1000) % 60);
-            } else {
-                if i < app.current_section {
-                    current_str = "-:--".to_owned();
-                    current_total_str = "-:--".to_owned();
-                } else {
-                    current_str = "".to_owned();
-                    current_total_str = "".to_owned();
-                }
-            };
-
+        for (i, s) in app.sections.iter().enumerate() {
             engine.print(
                 0,
                 i as i32 + 2,
                 &format!(
-                    "{:8}{:6}{:8}{:7}{}",
-                    name, pb_str, delta_str, current_str, current_total_str
+                    "{:8}{}  {} {}  {}  {}",
+                    s.name,
+                    app.pb_total_time(i),
+                    app.current_section_time(i),
+                    app.delta_section_time(i),
+                    app.current_total_time(i),
+                    app.delta_total_time(i),
                 ),
             );
         }
